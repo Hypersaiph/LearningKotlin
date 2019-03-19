24c3c25188587aa62e744a700f63fc223e9fe79a
Geovani <mygvs.mh@gmail.com>
2019-03-19 16:37:39 -0400

    operations

---
+++ src/NumbersAndOperators.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    val a = 12      // int
+    val b = 25    //double #kinda :v it was!
+
+    // + - / *
+    println(b + a)
+    println(b - a)
+    println(b / a)
+    println(b * a)
+
+    println(b % a)
+}
