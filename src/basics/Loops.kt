8950a4a87ae1a4f178f2b82f41c11a29fb53e484
gmendozah <geovanimendoza.h@gmail.com>
2020-03-16 23:55:26 -0400

    looping through arrayListOf() and hashMapOf()

--- src/basics/Loops.kt
+++ src/basics/Loops.kt
@@ -1,5 +1,14 @@
 package basics
 
 fun main(args: Array<String>) {
+    val starks = arrayListOf("Ned", "Robb", "Bran", "Sansa", "Catelyn")
+    val castlesByFamily = hashMapOf("stark" to "Winterfell", "tyrell" to "Highgarden", "tully" to "Riverrun", "lannister" to "Casterly Rock")
 
+    for (name in starks) {
+        println("$name Stark")
+    }
+
+    for ((key, value) in castlesByFamily) {
+        println("$value belongs to the $key family")
+    }
 }
